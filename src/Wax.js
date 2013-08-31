d90d364a05b8bb67269e41923f0639f01feeb81f
rmunson <Russell.Munson@mlb.com>
2013-08-30 22:58:04 -0400

    Some formatting, and error messaging in case a helper is registered more than once

--- src/Wax.js
+++ src/Wax.js
@@ -1,156 +1,155 @@
 /**
- * 	Wax : A template wrapper and manager for some 
- * 		popular JS implementations of the Mustache spec.
- * 	@version 0.2
+ *  Wax : A template wrapper and manager for some 
+ *      popular JS implementations of the Mustache spec.
+ *  @version 0.2
  */
 
 (function(ns,doc,T){
 
-	var primary,
-		EMPTY 	= '',
-		FN 		= 'function',
-		ERROR 	= 'Wax error : ',
-		ERROR_TEMPLATE	= ERROR + 'attempting to call unregistered template ',
-		ERROR_HELPER	= ERROR + 'attempting to register helper with an existing id ',
-		ccache	= {},
-		hcache	= {},
+    var primary,
+        EMPTY   = '',
+        FN      = 'function',
+        ERROR   = 'Wax error : ',
+        ERROR_TEMPLATE  = ERROR + 'attempting to call unregistered template ',
+        ERROR_HELPER    = ERROR + 'attempting to register helper with an existing id ',
+        ccache  = {},
+        hcache  = {},
 
-	_noop	= function(){},
-	_err	= function(ERR,id){
-		throw new Error(ERR+id);
-		return false;
-	},
-	isFunc 	= function(nf){
-		return typeof nf===FN;
-	},
+    _noop   = function(){},
+    _err    = function(ERR,id){
+        throw new Error(ERR+id);
+        return false;
+    },
+    isFunc  = function(nf){
+        return typeof nf===FN;
+    },
 
-	/**
-	 * Generate an error and return a no-operation function def to avoid
-	 * additional errors.
-	 * @param  {string} id ID associated with the failed operation
-	 * @return {function}    Empty function
-	 */
-	error = function(id){
-		return _err(ERROR_TEMPATE,id) || _noop;
-	},
+    /**
+     * Generate an error and return a no-operation function def to avoid
+     * additional errors.
+     * @param  {string} id ID associated with the failed operation
+     * @return {function}    Empty function
+     */
+    error = function(id){
+        return _err(ERROR_TEMPATE,id) || _noop;
+    },
 
-	/**
-	 * Add a compiled template to the compile cache ccache.
-	 * @param  {string} id  Key used for storage/retrieval of a compiled template
-	 * @param  {function} tmpl Compiled template
-	 * @return {function}     Compiled template stored during the operation
-	 */
-	store = function(id,tmpl){
-		return ccache[id]=ccache[id]||tmpl;
-	},
+    /**
+     * Add a compiled template to the compile cache ccache.
+     * @param  {string} id  Key used for storage/retrieval of a compiled template
+     * @param  {function} tmpl Compiled template
+     * @return {function}     Compiled template stored during the operation
+     */
+    store = function(id,tmpl){
+        return ccache[id]=ccache[id]||tmpl;
+    },
 
-	/**
-	 * Retrieve a registered template from the cache
-	 * @param  {string} id Named template key to retrieve
-	 * @return {function}    Compiled template or a no-operation function
-	 */
-	lookup = function(id){
-		return get(id)|| error(id);
-	},
+    /**
+     * Retrieve a registered template from the cache
+     * @param  {string} id Named template key to retrieve
+     * @return {function}    Compiled template or a no-operation function
+     */
+    lookup = function(id){
+        return get(id)|| error(id);
+    },
 
-	/**
-	 * More aggressive retrieve. Pull a registered template from the cache or 
-	 * attempt to find it in the current document.
-	 * @param  {string} id Named template key to retrieve
-	 * @return {function}    Compiled template or a no-operation function
-	 */
-	get = function(id){
-		return ccache[id] || registerById(id) || error(id);
-	},
+    /**
+     * More aggressive retrieve. Pull a registered template from the cache or 
+     * attempt to find it in the current document.
+     * @param  {string} id Named template key to retrieve
+     * @return {function}    Compiled template or a no-operation function
+     */
+    get = function(id){
+        return ccache[id] || registerById(id) || error(id);
+    },
 
-	getRawPartials = function(pars){
-		var key,
-			ret={},
-			par;
-		for(key in pars){
-			par=pars[key];
-			ret[key] = typeof par===FN && par.__nowax__ || par;
-		}
-		return ret;
-	},
-	compile=(function(t,co){
-		var c;
-		for(var key in t){
-			if(t[key] && t.hasOwnProperty(key) && co.hasOwnProperty(key)){
-				c=co[key];
-				primary=key;
-				return function(id,tmpl){
-					return ccache[id||tmpl] || c(id,tmpl);
-				}
-			}
-		}
-	})(T,{
-		mustache : function(_id,tmpl){
-			var tmp=isFunc(tmpl) && tmpl || Mustache.compile(tmpl),
-				fn=function(ctx,partials){
-					attachHelpers(ctx);
-					return tmp(ctx,partials)
-				};
-			fn.__nowax__=tmp;
-			return store(_id||tmpl,fn);
-		},
+    getRawPartials = function(pars){
+        var key,
+            ret={},
+            par;
+        for(key in pars){
+            par=pars[key];
+            ret[key] = typeof par===FN && par.__nowax__ || par;
+        }
+        return ret;
+    },
+    compile=(function(t,co){
+        var c;
+        for(var key in t){
+            if(t[key] && t.hasOwnProperty(key) && co.hasOwnProperty(key)){
+                c=co[key];
+                primary=key;
+                return function(id,tmpl){
+                    return ccache[id||tmpl] || c(id,tmpl);
+                }
+            }
+        }
+    })(T,{
+        mustache : function(_id,tmpl){
+            var tmp=isFunc(tmpl) && tmpl || Mustache.compile(tmpl),
+                fn=function(ctx,partials){
+                    attachHelpers(ctx);
+                    return tmp(ctx,partials)
+                };
+            fn.__nowax__=tmp;
+            return store(_id||tmpl,fn);
+        },
 
-		hogan : function(_id,tmpl){
-			var tmp=isFunc(tmpl) && new Hogan.Template(tmpl) ||Hogan.compile(tmpl),
-			fn=function(ctx,partials){
-				var opts;
-				if(partials){
-					opts=getRawPartials(partials);
-				}
-				attachHelpers(ctx);
-				return tmp.render(ctx,opts);
-			};
-			fn.__nowax__=tmp;
-			return store(_id||tmpl,fn);
-		}
-	}),
-	attachHelpers = function(ctx){
-		var key;
-		for(key in hcache){
-			ctx[key]=ctx[key]||hcache[key];
-		}
-	},
-	register = function(tmpl,id){
-		return compile(id,tmpl);
-	},
-	registerById = function(id){
-		var tmpl;
-		if(typeof id==="string"){
-			tmpl=(doc.getElementById(id)||EMPTY).innerHTML;
-			return tmpl && compile(id,tmpl);
-		}
-		return error(id);
-	},
-	registerHelper=function(id,fn){
-		hcache[id]=fn;
-		return true;
-	},
-	render = function(id,view,opts){
-		return get(id)(view,opts);
-	};
+        hogan : function(_id,tmpl){
+            var tmp=isFunc(tmpl) && new Hogan.Template(tmpl) ||Hogan.compile(tmpl),
+            fn=function(ctx,partials){
+                var opts;
+                if(partials){
+                    opts=getRawPartials(partials);
+                }
+                attachHelpers(ctx);
+                return tmp.render(ctx,opts);
+            };
+            fn.__nowax__=tmp;
+            return store(_id||tmpl,fn);
+        }
+    }),
+    attachHelpers = function(ctx){
+        var key;
+        for(key in hcache){
+            ctx[key]=ctx[key]||hcache[key];
+        }
+    },
+    register = function(tmpl,id){
+        return compile(id,tmpl);
+    },
+    registerById = function(id){
+        var tmpl;
+        if(typeof id==="string"){
+            tmpl=(doc.getElementById(id)||EMPTY).innerHTML;
+            return tmpl && compile(id,tmpl);
+        }
+        return error(id);
+    },
+    registerHelper=function(id,fn){
+        return hcache[id] && _err(ERROR_HELPER,id) || !!(hcache[id]=fn);
+    },
+    render = function(id,view,opts){
+        return get(id)(view,opts);
+    };
 
-	ns.Wax={
-		get 			: get,
-		lookup 			: lookup,
-		register 		: register,
-		registerById	: registerById,
-		registerHelper  : registerHelper,
-		render 			: render,
-		list 			: function(){
-			return ccache;
-		}
-	}
+    ns.Wax={
+        get             : get,
+        lookup          : lookup,
+        register        : register,
+        registerById    : registerById,
+        registerHelper  : registerHelper,
+        render          : render,
+        list            : function(){
+            return ccache;
+        }
+    }
 
 })(this,document,{
-	mustache 	: typeof Mustache!=='undefined' && Mustache,
-	hogan 		: typeof Hogan!=='undefined' && Hogan
+    mustache    : typeof Mustache!=='undefined' && Mustache,
+    hogan       : typeof Hogan!=='undefined' && Hogan
 });
 
 if(this.define && this.define.amd){
-	define('Wax',this.Wax)
+    define('Wax',this.Wax)
 }
