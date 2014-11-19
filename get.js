003eef461f4822ce0088a07b1f2aea37c3fc80da
Rusell Munson <russ.munson@gmail.com>
2014-11-19 13:01:01 -0500

    allow .mustache to be left off url - set as default ext

--- get.js
+++ get.js
@@ -1,28 +1,33 @@
 define(['text','./Wax'],function(text,Wax){
-    var buildOutput = {};
+    var MUSTACHE="mustache",
+        buildOutput = {};
 
     return {
-        load : function(fileName, parentRequire, onLoad, config){
+        load : function(module, parentRequire, onLoad, config){
+            var file = text.parseName(module),
+                fileName=file.moduleName + '.'+(file.ext||MUSTACHE);
+            console.info('load',fileName,module);
             text.get(parentRequire.toUrl(fileName), function(templateString){
                 var tpl;
                 // If we're in an rjs optimizer routine, compile for Hogan, and
                 // return the string for mustache
                 if(config.isBuild){
-                    tpl=buildOutput[fileName]=Wax.compile.isHogan ? 
+                    tpl=buildOutput[module]=Wax.compile.isHogan ? 
                         Wax.WaxMustache.compile(templateString,{asString:true}) :
                         '"'+text.jsEscape(templateString) + '"';
-                }else{
-                    tpl=Wax.register(fileName,templateString);
+                } else {
+                    tpl=Wax.register(module,templateString);
                 }
                 onLoad(tpl);
             });
         },
-        write : function(pluginName, fileName, write){
-            var out = 'define("'+pluginName+'!"'+fileName+',["Wax/Wax"],function(Wax){' +
-                'return Wax.register("'+fileName+'",'+
-                    buildOutput[fileName] +
+        write : function(pluginName, module, write){
+            var out = 'define("'+pluginName+'!"'+module+',["Wax/Wax"],function(Wax){' +
+                'return Wax.register("'+module+'",'+
+                        buildOutput[module] +
                     ');' +
-                '});'
+                '});';
+            console.info('write',module);
             write(out);
         }
     }
